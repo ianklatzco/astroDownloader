767da514e07faeb96084d7a2d070ba78478b1d68
Ian Klatzco <iklatzco@gmail.com>
2016-04-10 23:50:23 -0500

    add some more to count.py to return largest collision

--- count.py
+++ count.py
@@ -1,5 +1,9 @@
-#i should go to be but alas, this is just too much fun
+#i should go to beD but alas, this is just too much fun
 with open('daysSetNoTimes.txt', 'r') as infile:
     dates = [x.strip() for x in infile.readlines()]
+    datesList = []
     for x in xrange(1,365):
+        datesList.append(dates.count(str(x)))
         print str(x) + ':' + str(dates.count(str(x)))
+    print( 'Largest collision: ' + str(max(datesList)) + ' : paste output into text file and cmd F to find the date')
+    print("#lazycoderislazy")
